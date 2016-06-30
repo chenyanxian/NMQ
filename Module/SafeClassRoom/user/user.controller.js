7764419cb453624b7da1ba154118061d829fa090
Stringbao <String_bao@163.com>
2016-06-30 21:17:24 +0800

    edit files

--- Module/SafeClassRoom/user/user.controller.js
+++ Module/SafeClassRoom/user/user.controller.js
@@ -47,39 +47,61 @@ angular.module('app')
             $scope.downs = enume.downs;
         }
 
-        var dragEl = document.querySelector("#dragEl");
-        function drop(e){
-            e.stopPropagation();
-            e.preventDefault();
-
-            var fileList = e.dataTransfer.files;
-            var oImg = document.createElement("img");
-            var render = new FileReader();
-
-            render.onerror = function(){
-                alert("读取文件失败!");
+        var fileContent = "";
+        var dragEl = document.querySelector("#fileLoad");
+        dragEl.addEventListener("change",function(e){
+            if(e.target.files.length == 0){
+                alert("请选择图片上传!");
+                return;
             }
-
-            render.onload = function(a){
-                oImg.src = this.result;
-                oImg.style.width = dragEl.style.width;
-                oImg.style.height = dragEl.style.height;
-                dragEl.innerHTML = "";
-                document.querySelector("#dragEl").appendChild(oImg);
-                console.log("ajax",111);
+            var file = e.target.files[0];
+            var filereader = new FileReader();
+            filereader.onload = function () {
+                fileContent = this.result;
             }
+            filereader.readAsDataURL(file);
+        },false);
 
-            render.readAsDataURL(fileList[0]);
+        $scope.createUser = function(){
+            var tmp = {
+                gh:$scope.gh,       //工号
+                cym:$scope.cym,     //曾用名
+                xm:$scope.xm,       //姓名
+                ywxm:$scope.ywxm,   //英文姓名
+                xmpy:$scope.xmpy,   //姓名拼音
+                sex:$scope.sex,     //性别
+                csrq:$scope.csrq,   //出生日期
+                placeNum:$scope.placeNum,  //籍贯
+                nationalityNum:$scope.nationalityNum,   //国籍
+                nationNum:$scope.nationNum,             //民族
+                provincesNum:$scope.provincesNum,       //省
+                citysNum:$scope.citysNum,               //市
+                downsNum:$scope.downsNum,               //区
+                idTypeNum:$scope.idTypeNum,             //证件类型
+                zjhm:$scope.zjhm,                       //证件号码
+                maritalStatusNum:$scope.maritalStatusNum,//婚姻状况
+                macaoNum:$scope.macaoNum,                //港澳台外
+                zp:fileContent                          //照片
+            };
+
+            $http({
+                method:"POST",
+                url:"data.json",
+                data:tmp
+            }).success(function(d){
+                if(d.status.code == "1"){
+                    alert("保存成功");
+                    $state.go("safeRoom.userManagement");
+                }else{
+                    alert(d.status.message);
+                }
+            })
+
         }
 
-        dragEl.addEventListener("dragstart",function(){},false);
-        dragEl.addEventListener("drop",function(e){
-            drop(e);
-        },false);
-        dragEl.addEventListener("dragover",function(e){
-            e.stopPropagation();
-            e.preventDefault();
-        },false);
+        $scope.cancelUser = function(){
+            $state.go("safeRoom.userManagement");
+        }
 
     })
     .controller('stuManagementCtrl',function($http,$scope,$state){
