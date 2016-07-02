13f78ec1a3cf1b3379a8241176e4e59523a30dc3
chenyanxian <18600467611@163.com>
2016-07-02 23:10:54 +0800

    edit
    edit

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ angular.module("app",[
     .config(function($stateProvider,$urlRouterProvider,$locationProvider,$httpProvider){
         $urlRouterProvider.otherwise('/safeRoom/templateList');
 
+
         //$locationProvider.html5Mode(true);
 
         //$httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
