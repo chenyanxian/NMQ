210c6783a4c03d4508bf354155f5a2bb7e62a983
Stringbao <String_bao@163.com>
2016-07-04 15:56:29 +0800

    edit

--- Module/SafeClassRoom/analysis/analysis.controller.js
+++ Module/SafeClassRoom/analysis/analysis.controller.js
@@ -384,8 +384,7 @@ angular.module('app')
 
         $scope.id = $stateParams.id;
 
-        $scope.data = [
-            {
+        $scope.data = {
                 templateCategory: "1",
                 templateType: "2",
                 title: "安全问卷调查哟",
@@ -399,23 +398,29 @@ angular.module('app')
                             {
                                 code:"2_1",
                                 "cate": "radio",
-                                value:"A",
+                                value:"A-B-C-D-A,B-C",
                                 "name": "小伙子们，你们速度真是杠杠的啊",
                                 "sort": 1,
+                                "bz":"C",
+                                "scores":10
                             },
                             {
                                 code:"2_2",
                                 "cate": "pingfen",
                                 "name": "你对自己的长相打多少分?",
                                 "sort": 2,
-                                value:"40"
+                                value:"40-20-10-20-10-10",
+                                "bz":"C",
+                                "scores":10
                             },
                             {
                                 "cate": "checkbox",
                                 code:"2_3",
                                 "name": "我就问你，你是不是傻？",
                                 "sort": 3,
-                                value:"A,B"
+                                value:"A,B-C-C-C-A-D",
+                                "bz":"C",
+                                "scores":10
                             }
                         ]
                     },
@@ -433,21 +438,27 @@ angular.module('app')
                                 "items": [
                                     {
                                         code:"1-3_1",
-                                        "value": "张三",
+                                        "value": "张三-李四-王五-张三-李四-王五",
                                         "name": "姓名",
-                                        "sort": 1
+                                        "sort": 1,
+                                        "bz":"C",
+                                        "scores":10
                                     },
                                     {
                                         code:"1-3_2",
-                                        "value": "20",
+                                        "value": "20-40-20-10-20-10",
                                         "name": "年龄",
-                                        "sort": 2
+                                        "sort": 2,
+                                        "bz":"C",
+                                        "scores":10
                                     },
                                     {
                                         code:"1-3_3",
-                                        "value": "18611701867",
+                                        "value": "18611701867-18611701867-18611701867-18611701867-18611701867-18611701867",
                                         "name": "手机号码",
-                                        "sort": 3
+                                        "sort": 3,
+                                        "bz":"C",
+                                        "scores":10
                                     }
                                 ]
                             },
@@ -455,168 +466,15 @@ angular.module('app')
                                 code:"1_2",
                                 "cate": "radio",
                                 "name": "性别",
-                                value:"男",
-                                "sort": 2
+                                value:"男-女-男-女-男-女",
+                                "sort": 2,
+                                "bz":"C",
+                                "scores":10
                             },
                         ]
                     }
                 ]
-            },
-            {
-                templateCategory: "1",
-                templateType: "2",
-                title: "安全问卷调查哟",
-                random: "",
-                content: "一起来参与，赢大奖!",
-                "data": [
-                    {
-                        "title": "第二章节",
-                        code:"2",
-                        "tms": [
-                            {
-                                code:"2_1",
-                                "cate": "radio",
-                                value:"A",
-                                "name": "小伙子们，你们速度真是杠杠的啊",
-                                "sort": 1,
-                            },
-                            {
-                                code:"2_2",
-                                "cate": "pingfen",
-                                "name": "你对自己的长相打多少分?",
-                                "sort": 2,
-                                value:"40"
-                            },
-                            {
-                                "cate": "checkbox",
-                                code:"2_3",
-                                "name": "我就问你，你是不是傻？",
-                                "sort": 3,
-                                value:"A,B"
-                            }
-                        ]
-                    },
-                    {
-                        "title": "第一章节",
-                        "code":"1",
-                        "tms": [
-                            {
-                                code:"1-3",
-                                "cate": "textbox",
-                                "bida": true,
-                                "wtjtt": false,
-                                "name": "个人基本信息",
-                                "sort": 3,
-                                "items": [
-                                    {
-                                        code:"1-3_1",
-                                        "value": "张三1",
-                                        "name": "姓名",
-                                        "sort": 1
-                                    },
-                                    {
-                                        code:"1-3_2",
-                                        "value": "220",
-                                        "name": "年龄",
-                                        "sort": 2
-                                    },
-                                    {
-                                        code:"1-3_3",
-                                        "value": "182222611701867",
-                                        "name": "手机号码",
-                                        "sort": 3
-                                    }
-                                ]
-                            },
-                            {
-                                code:"1_2",
-                                "cate": "radio",
-                                "name": "性别",
-                                value:"男",
-                                "sort": 2
-                            },
-                        ]
-                    },
-                ]
-            },
-            {
-                templateCategory: "1",
-                templateType: "2",
-                title: "安全问卷调查哟",
-                random: "",
-                content: "一起来参与，赢大奖!",
-                "data": [
-                    {
-                        "title": "第二章节",
-                        code:"2",
-                        "tms": [
-                            {
-                                code:"2_1",
-                                "cate": "radio",
-                                value:"A,B",
-                                "name": "小伙子们，你们速度真是杠杠的啊",
-                                "sort": 1,
-                            },
-                            {
-                                code:"2_2",
-                                "cate": "pingfen",
-                                "name": "你对自己的长相打多少分?",
-                                "sort": 2,
-                                value:"409"
-                            },
-                            {
-                                "cate": "checkbox",
-                                code:"2_3",
-                                "name": "我就问你，你是不是傻？",
-                                "sort": 3,
-                                value:"A,B,D"
-                            }
-                        ]
-                    },
-                    {
-                        "title": "第一章节",
-                        "code":"1",
-                        "tms": [
-                            {
-                                code:"1-3",
-                                "cate": "textbox",
-                                "bida": true,
-                                "wtjtt": false,
-                                "name": "个人基本信息",
-                                "sort": 3,
-                                "items": [
-                                    {
-                                        code:"1-3_1",
-                                        "value": "张三2",
-                                        "name": "姓名",
-                                        "sort": 1
-                                    },
-                                    {
-                                        code:"1-3_2",
-                                        "value": "2330",
-                                        "name": "年龄",
-                                        "sort": 2
-                                    },
-                                    {
-                                        code:"1-3_3",
-                                        "value": "1863333311701867",
-                                        "name": "手机号码",
-                                        "sort": 3
-                                    }
-                                ]
-                            },
-                            {
-                                code:"1_2",
-                                "cate": "radio",
-                                "name": "性别",
-                                value:"女",
-                                "sort": 2
-                            },
-                        ]
-                    }
-                ]
-            }
-        ];
+            };
 
         function getHeadInfo(data){
             var d = data.data;
@@ -648,44 +506,62 @@ angular.module('app')
             }
             return {cols:colSpans,wtTitles:wtTitles};
         }
-        var tmp = getHeadInfo($scope.data[0]);
+
+        var tmp = getHeadInfo($scope.data);
+
         $scope.colSpan = tmp.cols;
         $scope.wtTitles = tmp.wtTitles;
-
+        $scope.scoresShow = false;
+        $scope.details = [];
+        $scope.wentis = []
 
         function getAllWentis(data){
-            var wentis = [];
+
+            var tmpRes = {wentis:[],res:[]};
 
             for(var i=0;i< data.length;i++){
-
-                var _d = data[i].data;
-                var tmp = {children:[]};
-
-
-                for(var j =0;j<_d.length;j++){
-                    var tms = _d[j].tms;
-
-                    for(var k=0;k<tms.length;k++){
-                        if(tms[k].cate == "textbox"){
-                            var items = tms[k].items;
-                            for(var u=0;u<items.length;u++){
-                                tmp.children.push(items[u].value);
-                            }
-                        }else{
-                            tmp.children.push(tms[k].value);
+                var tms = data[i].tms;
+                for(var k=0;k<tms.length;k++){
+                    if(tms[k].cate == "textbox"){
+                        var items = tms[k].items;
+                        for(var u=0;u<items.length;u++){
+                            tmpRes.wentis.push(items[u]);
                         }
+                    }else{
+                        tmpRes.wentis.push(tms[k]);
                     }
                 }
-
-
-
-                wentis.push(tmp);
             }
-            return wentis;
+
+            var len = tmpRes.wentis[0].value.split('-').length;
+            //scores
+            for(var i=0;i<len;i++){
+                var tmp = {children:[]};
+                for(var k=0;k<tmpRes.wentis.length;k++){
+                    tmp.children.push({name:tmpRes.wentis[k].value.split('-')[i]});
+                }
+                tmpRes.res.push(tmp);
+            }
+
+            return tmpRes;
         }
 
-        $scope.wentis = getAllWentis($scope.data);
+        enume.getData("/cmsapi/template/statistics?code="+$scope.id,function(d){
+            if(d.templateType == "kaoti"){
+                $scope.scoresShow = true;
+            }else{
+                $scope.scoresShow = false;
+            }
 
+            $scope.data = d;
+            var res = getAllWentis(d);
+            $scope.details = res.res;
+            $scope.wentis = res.wentis;
+        })
+
+        var res = getAllWentis($scope.data.data);
+        $scope.details = res.res;
+        $scope.wentis = res.wentis;
     })
     .controller('lessonsListCtrl', function ($http,$scope,enume,$state) {
 
